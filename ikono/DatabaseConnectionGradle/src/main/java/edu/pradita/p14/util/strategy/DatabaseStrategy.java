2fc4e644c75f157a5e95f9191e3614d0e0d9a315
Khiar Zaki Maulana <74346237+khiarj4ki@users.noreply.github.com>
2025-06-09 16:04:00 +0700

    Delete ikono/DatabaseConnectionGradle directory

--- ikono/DatabaseConnectionGradle/src/main/java/edu/pradita/p14/util/strategy/DatabaseStrategy.java
+++
@@ -1,7 +0,0 @@
-package edu.pradita.p14.util.strategy;
-
-import java.util.Properties;
-
-public interface DatabaseStrategy {
-    Properties getProperties(String host, String port, String dbName, String username, String password);
-}
