4685455f3e7bf9ae833e578666638f3c412fd906
Deria <dewscy89@gmail.com>
2025-06-11 14:41:52 +0700

    Update PurcaseTransaction.java

--- ikono/src/edu/pradita/p14/transaksiPembelianFolder/app/src/main/java/com/project/model/PurcaseTransaction.java
+++ ikono/src/edu/pradita/p14/transaksiPembelianFolder/app/src/main/java/com/project/model/PurcaseTransaction.java
@@ -19,4 +19,62 @@ public class PurchaseTransaction {
 
     @OneToMany(mappedBy = "transaction", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PurchaseDetail> details = new ArrayList<>();
+
+    // ==== GETTERS & SETTERS ====
+
+    public int getPurchaseID() {
+        return purchaseID;
+    }
+
+    public void setPurchaseID(int purchaseID) {
+        this.purchaseID = purchaseID;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public int getVendorID() {
+        return vendorID;
+    }
+
+    public void setVendorID(int vendorID) {
+        this.vendorID = vendorID;
+    }
+
+    public List<PurchaseDetail> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<PurchaseDetail> details) {
+        this.details = details;
+    }
 }
